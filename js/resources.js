d2feb3612a958704f46675fd05f33d3ba2ad40af
unknown <dimau5117@gmail.com>
2025-04-21 01:24:45 +0300

    medium version with js

--- js/resources.js
+++ js/resources.js
@@ -2,3 +2,27 @@ let elements = document.querySelectorAll('.references a');
 elements.forEach(element => {
     element.setAttribute('target', '_blank');
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+var img1 = document.getElementById('img1');
+var img2 = document.getElementById('img2');
+
+const listItems = document.querySelectorAll('.references li');
+
+
+        listItems[0].addEventListener('mouseenter', () => {
+            img1.style.cssText = "margin-bottom: 50px;";
+        });
+        listItems[0].addEventListener('mouseleave', () => {
+            img1.style.cssText = "margin-bottom: 0px;";
+        });
+
+
+        listItems[1].addEventListener('mouseenter', () => {
+            img2.style.cssText = "margin-bottom:50px;";
+        });
+        listItems[1].addEventListener('mouseleave', () => {
+             img2.style.cssText = "margin-bottom:0px;";
+        });	
+});
